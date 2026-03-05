60f6d00c128ea9e23114af52de639787faaec979
solonovamax <solonovamax@12oclockpoint.com>
2026-03-05 15:02:39 -0500

    Fix thorns enchantment
    Signed-off-by: solonovamax <solonovamax@12oclockpoint.com>

--- src/main/java/org/jlortiz/playercollars/mixin/MixinEnchantmentHelper.java
+++ src/main/java/org/jlortiz/playercollars/mixin/MixinEnchantmentHelper.java
@@ -2,12 +2,21 @@ package org.jlortiz.playercollars.mixin;
 
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import com.llamalad7.mixinextras.sugar.Local;
+import dev.emi.trinkets.api.TrinketsApi;
 import net.minecraft.enchantment.Enchantment;
 import net.minecraft.enchantment.EnchantmentHelper;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.Pair;
+import org.jlortiz.playercollars.PlayerCollarsMod;
 import org.jlortiz.playercollars.item.CollarItem;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.Collection;
 
 @Mixin(EnchantmentHelper.class)
 public class MixinEnchantmentHelper {
@@ -17,5 +26,25 @@ public class MixinEnchantmentHelper {
     )
     private static boolean isAcceptableItem(boolean original, int power, ItemStack stack, boolean treasureAllowed, @Local Enchantment enchantment) {
         return original || ((stack.getItem() instanceof CollarItem) && CollarItem.isAcceptableEnchantment(enchantment));
+    }
+
+    @Inject(
+            method = "onUserDamaged",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/enchantment/EnchantmentHelper;forEachEnchantment(Lnet/minecraft/enchantment/EnchantmentHelper$Consumer;Lnet/minecraft/item/ItemStack;)V"
+            )
+    )
+    private static void applyCollarThorns(LivingEntity user, Entity attacker, CallbackInfo ci, @Local EnchantmentHelper.Consumer consumer) {
+        var stacks = TrinketsApi.getTrinketComponent(user)
+                .map((x) -> x.getEquipped((y) -> y.isIn(PlayerCollarsMod.COLLAR_TAG)))
+                .stream()
+                .flatMap(Collection::stream)
+                .map(Pair::getRight)
+                .toList();
+
+        System.out.println("is player, checking thorns...");
+
+        EnchantmentHelper.forEachEnchantment(consumer, stacks);
     }
 }
