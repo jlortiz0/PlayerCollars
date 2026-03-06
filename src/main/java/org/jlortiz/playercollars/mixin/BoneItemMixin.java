8b16864dc247b7661148685ceee4b91535d550b3
solonovamax <solonovamax@12oclockpoint.com>
2026-03-06 01:34:55 -0500

    Make bones able to be placed in head slot
    Signed-off-by: solonovamax <solonovamax@12oclockpoint.com>

--- src/main/java/org/jlortiz/playercollars/mixin/BoneItemMixin.java
+++ src/main/java/org/jlortiz/playercollars/mixin/BoneItemMixin.java
@@ -6,10 +6,18 @@ import org.jlortiz.playercollars.item.DummyBoneItem;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.Slice;
 
 @Mixin(Items.class)
 public class BoneItemMixin {
-    @Redirect(method="<clinit>", at=@At(value="NEW", target = "net/minecraft/item/Item", ordinal=44))
+    @Redirect(
+            method = "<clinit>",
+            at = @At(value = "NEW", target = "net/minecraft/item/Item"),
+            slice = @Slice(
+                    from = @At(value = "CONSTANT", args = "stringValue=bone"),
+                    to = @At(value = "CONSTANT", args = "stringValue=sugar")
+            )
+    )
     private static Item makeEquippableBone(Item.Settings settings) {
         return new DummyBoneItem(settings);
     }
